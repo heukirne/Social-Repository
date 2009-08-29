ce72eec3490f39f18eebbc6a47a2c5001566fffb
Henrique Dias <heukirne@gmail.com>
2009-08-29 13:21:02 -0300

    more engines, better regex

--- save_wsdl_url.php
+++ save_wsdl_url.php
@@ -4,18 +4,35 @@
 
 	echo "<pre>";
 	for ($i=0;$i<=600;$i+=50) {
-		$urlExtra = "";
-		$query = "filetype:wsdl";
-		$wsdl_list = file_get_contents("http://www.google.com/search?q=$query&start=$i&filter=0&num=50$urlExtra");
+		$wsdl_list = "";
+		
+		//WSDL in Search Engines (Google, Bing, Yahoo, AltaVista, Ask)
+		if (!empty($_GET['cr'])) $urlExtra = "&cr=country".$_GET['cr'];
+		$query = "filetype:asmx"; //"allinurl:asmx?wsdl+filetype:asmx"; //"filetype:wsdl";
+		$url = "http://www.google.com.br/search?q=$query&start=$i&filter=0&num=50$urlExtra"; //to 600 step 50 (with flood block =/)
+		$url = "http://www.bing.com/search?q=asmx&first=$i"; // to 1000 step 10
+		$url = "http://search.yahoo.com/search?p=inurl:asmx&b=$i"; //to 1000 step 10
+		$url = "http://www.altavista.com/web/results?aqmode=s&aqa=wsdl&rc=url&lh=asmx&nbq=50&stq=$i"; //to 1000 step 50
+		$url = "http://www.ask.com/web?q=inurl:asmx&page=$i"; //to 30 step 1
+		
+		//Get URL Contents
+		$wsdl_list = file_get_contents($url);
 		if (empty($wsdl_list)) die("ERROR!!");
-		preg_match_all("/\shref\=\"(http\:\/\/[^\"]*\.wsdl)\"\s/", $wsdl_list, $matches);
+		
+		//Regex for wsdl and asmx
+		preg_match_all("/href\=[\"\'](https?\:\/\/[^\"\']*\.asmx)[\"\'\?]/", strtolower($wsdl_list), $matches);
 		echo "</br>start=$i";
-		//print_r($matches[1]);
+		//print_r($matches[1]); echo $wsdl_list; exit();
+		
+		
+		//Save URL in Database
 		foreach ($matches[1] as $iten) {
-			$db->db_insert("webservice","url = '$iten'");
+			$db->db_insert("webservice","url = '$iten?wsdl'"); //add ?wsdl for only asmx search
 		}
 		echo "<script>document.title = 'New WSDL: {$db->affected_rows}';</script>";
 		flush();
+		exit;
 	}
 	echo "</pre>";
+	
 ?>
